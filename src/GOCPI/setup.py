e82b1838e6798cb00b2a069607e9ac6efa826fcf
Connor <connormcdowall@gmail.com>
2020-07-23 19:30:12 +1200

    Updated to get package working

--- src/GOCPI/setup.py
+++ src/GOCPI/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
-'Note: To upload new versions 1) cd to GOCPI_Functions 2) python setup.py sdist 3) twine upload dist/*'
+'Note: To upload new versions 1) cd to GOCPI 2) python setup.py sdist 3) twine upload dist/*'
 'Note: To download 1) pip install --upgrade GOCPI'
 'Note: Make your own python package: https://towardsdatascience.com/make-your-own-python-package-6d08a400fc2d'
 'Note: PyPi https://pypi.org/manage/project/gocpi-functions/releases/#modal-close'
 
 setup(name='GOCPI',
-      version='1.10',
+      version='1.00',
       description='User Defined Functions and Energy Systems Data for GOCPI Project',
       packages=['GOCPI'],
       author_email='connormcdowall@gmail.com',
--- src/GOCPI_Functions/GOCPI_Functions/__init__.py
+++
@@ -1,2 +0,0 @@
-from .Navigation import Navigation
-from .Energysystems import Energy_Systems
